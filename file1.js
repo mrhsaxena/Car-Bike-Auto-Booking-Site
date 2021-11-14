0f2b603a69767ecfe7a7c3cf1c80502e96c40ddf
mrhsaxena <harshit.saxena@creatiosoft.com>
2021-11-14 19:12:48 +0530

    coding

---
+++ file1.js
@@ -0,0 +1,30 @@
+console.log("yes")
+
+
+console.log("yes")
+
+console.log("yes")
+
+
+console.log("yes")
+
+
+console.log("yes")
+
+
+console.log("yes")
+
+
+var express  = require('express')
+var app  = express()
+var server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is started on '+ (process.env.PORT || 3000))
+}) 
+
+
+let x = "harshit              "
+let new$string = x.trim()
+console.log(new$string.length)
+x = 1
+
+console.log(typeof x)
